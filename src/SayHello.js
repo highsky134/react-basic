e6cdedd9c61ecd5a6f4d88c20d52b86eb2f18a9d
highsky134 <sangchun134@gmail.com>
2023-05-22 10:40:57 +0900

    # 1. 컴포넌트 기초

---
+++ src/SayHello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// 컴포넌트
+export default function SayHello() {
+    return (
+        <div>
+            <p>안녕</p>
+            <span>메롱</span>
+        </div>
+    );
+}
